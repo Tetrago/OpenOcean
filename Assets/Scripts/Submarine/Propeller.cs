2dafade03127f307586062f910f89c0720ef6038
Geiss James <89.VV@pcsb.org>
2019-09-17 13:54:02 -0400

    Submarine improvements.

---
+++ Assets/Scripts/Submarine/Propeller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Submarine
+{
+    public class Propeller : MonoBehaviour
+    {
+        public Transform propeller_;
+        public float targetSpeed_ = 0.2f;
+        public float smoothSpeed_ = 0.2f;
+
+        private float currentSpeed_;
+        private float velocity_;
+
+        private void Awake()
+        {
+            currentSpeed_ = targetSpeed_;
+        }
+
+        private void Update()
+        {
+            if(currentSpeed_ != targetSpeed_)
+                currentSpeed_ = Mathf.SmoothDamp(currentSpeed_, targetSpeed_, ref velocity_, smoothSpeed_);
+
+            Vector3 rot = propeller_.rotation.eulerAngles;
+            propeller_.rotation = Quaternion.RotateTowards(propeller_.rotation, Quaternion.Euler(rot + Vector3.forward * currentSpeed_), currentSpeed_ * Time.deltaTime);
+        }
+
+        public void SetTargetSpeed(float speed)
+        {
+            targetSpeed_ = speed;
+        }
+    }
+}
